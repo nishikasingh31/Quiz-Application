213640fcd379b6be7d49d398773e091fa94d49ac
Nishika Singh <nishikasingh0307@gmail.com>
2026-04-13 22:09:35 +0530

    Update api.js

--- Frontend/quiz-app/src/services/api.js
+++ Frontend/quiz-app/src/services/api.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'https://online-quiz-application-yffb.onrender.com/api';
+const API_BASE_URL = 'https://quiz-application-arye.onrender.com/api';
 
 // Fetch all available quizzes for the Menu
 export const getAllQuizzes = async () => {
